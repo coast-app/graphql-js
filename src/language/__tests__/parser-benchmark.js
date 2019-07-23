d75ae90880a5a7c75934c7dade25975ffe95c686
Jeremy Shimko <jeremy.shimko@gmail.com>
2019-07-23 11:33:47 -0400

    build with customn Int scalar

--- src/language/__tests__/parser-benchmark.js
+++
@@ -1,9 +0,0 @@
-// @flow strict
-
-import { kitchenSinkQuery } from '../../__fixtures__';
-import { parse } from '../parser';
-
-export const name = 'Parse kitchen sink';
-export function measure() {
-  parse(kitchenSinkQuery);
-}
